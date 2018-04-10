6d5239584ef285498f71e75415184cf271dd49f3
fab31 <fdev31@gmail.com>
2018-04-10 22:06:09 +0200

    remove not really applicable header

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,3 @@
-/***************************************************************************
-  This is a library for the APDS9960 digital proximity, ambient light, RGB, and gesture sensor
-
-  This sketch puts the sensor in proximity mode and enables the interrupt
-  to fire when proximity goes over a set value
-
-  Designed specifically to work with the Adafruit APDS9960 breakout
-  ----> http://www.adafruit.com/products/3595
-
-  These sensors use I2C to communicate. The device's I2C address is 0x39
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Dean Miller for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
-
-----------------------------------------------------------------------------
-
-Used as base to make some X-ring lamp
-
- ***************************************************************************/
-
 #include "Adafruit_APDS9960.h"
 #include <ESP8266WiFi.h>
 #include <Adafruit_NeoPixel.h>
